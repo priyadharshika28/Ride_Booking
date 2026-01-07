deb16b766a67ba6d1947f4f7e8c94e1a1d73beb2
priyadharshika28 <140396562+priyadharshika28@users.noreply.github.com>
2026-01-07 21:55:01 +0530

    Refactor Cards component to use selectedPayment

--- components/Booking/Cards.tsx
+++ components/Booking/Cards.tsx
@@ -2,24 +2,23 @@ import CardLists from '@/data/CardLists'
 import React, { useState } from 'react'
 import Image from 'next/image'
 
-function Cards() {
-  const [activeIndex,setActiveIndex]=useState<any>();
+function Cards({ selectedPayment, setSelectedPayment }: any) {
 
   return (
     <div className='mt-4'>
-        <h2 className='font-semibold mb-3'>Payment Methods</h2>
-        <div className='grid grid-cols-5 mt-4'>
-          {CardLists.map((item,index)=>(
-            <div key={index} className={`w-[50px] border-[1px]
+      <h2 className='font-semibold mb-3'>Payment Methods</h2>
+      <div className='grid grid-cols-5 mt-4'>
+        {CardLists.map((item, index) => (
+          <div key={index} className={`w-[50px] border-[1px]
             flex items-center justify-center rounded-md
             cursor-pointer hover:scale-110 transition-all hover:border-yellow-400 border-gray-300
-            ${activeIndex==index?'border-yellow-400 border-[2px]':null}`}
-            onClick={()=>setActiveIndex(index)}>
-              <Image src={item.image} alt={item.name}
+            ${selectedPayment?.name == item.name ? 'border-yellow-400 border-[2px]' : null}`}
+            onClick={() => setSelectedPayment(item)}>
+            <Image src={item.image} alt={item.name}
               width={30} height={50}></Image>
-            </div>
-          ))}
-        </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
