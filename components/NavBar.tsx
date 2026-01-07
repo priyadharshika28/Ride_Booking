a9469aaf87ddd2617fce845f31983576ce919ced
priyadharshika28 <140396562+priyadharshika28@users.noreply.github.com>
2026-01-07 12:06:38 +0530

    Update NavBar.tsx

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,10 +1,11 @@
 import { UserButton } from '@clerk/nextjs'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function NavBar() {
   return (
     <div className="flex justify-between items-center p-3 px-10 border-b-[1px] shadow-sm">
-      
+
       <div className="flex items-center gap-6">
         <Image
           src="/logo.webp"
@@ -14,9 +15,18 @@ export default function NavBar() {
         />
 
         <div className="flex gap-4">
-          <h2 className='hover:bg-gray-50 p-2 rounded-md cursor-pointer transition-all'>Home</h2>
-          <h2 className='hover:bg-gray-50 p-2 rounded-md cursor-pointer transition-all'>History</h2>
-          <h2 className='hover:bg-gray-50 p-2 rounded-md cursor-pointer transition-all'>Help</h2>
+          <Link href={'/'}>
+            <h2 className='hover:bg-gray-50 p-2 rounded-md cursor-pointer transition-all'>Home</h2>
+          </Link>
+          <Link href={'/history'}>
+            <h2 className='hover:bg-gray-50 p-2 rounded-md cursor-pointer transition-all'>History</h2>
+          </Link>
+          <Link href={'/help'}>
+            <h2 className='hover:bg-gray-50 p-2 rounded-md cursor-pointer transition-all'>Help</h2>
+          </Link>
+          <Link href={'/rewards'}>
+            <h2 className='hover:bg-gray-50 p-2 rounded-md cursor-pointer transition-all'>Rewards</h2>
+          </Link>
         </div>
       </div>
 
