55bb8fb8ca33d22c66ee15c9739f290a334cae85
priyadharshika28 <140396562+priyadharshika28@users.noreply.github.com>
2026-01-11 21:17:28 +0530

    Configure remotePatterns for images
    Added remotePatterns for image configuration.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com', // sometimes it varies, adding specific one just in case
+      }
+    ],
+  },
 };
 
 export default nextConfig;
