65877484b800d14f1c9d3588e22a84e8ab41a4d0
priyadharshika28 <140396562+priyadharshika28@users.noreply.github.com>
2026-01-11 20:58:33 +0530

    Format CarList.tsx for consistency

--- data/CarList.tsx
+++ data/CarList.tsx
@@ -1,34 +1,34 @@
 import { channel } from "diagnostics_channel";
 
-export default[
+export default [
     {
-        id:1,
-        name:'Economy',
-        image:'/car77.png',
-        price:1
+        id: 1,
+        name: 'Economy',
+        image: '/car77.png',
+        price: 1
     },
     {
-        id:2,
-        name:'MiniVan',
-        image:'/car66.png',
-        price:1.2
+        id: 2,
+        name: 'MiniVan',
+        image: '/car66.png',
+        price: 1.2
     },
     {
-        id:2,
-        name:'Comfort',
-        image:'/car55.png',
-        price:1.5
+        id: 3,
+        name: 'Comfort',
+        image: '/car55.png',
+        price: 1.5
     },
     {
-        id:4,
-        name:'Luxury',
-        image:'/car44.png',
-        price:2
+        id: 4,
+        name: 'Luxury',
+        image: '/car44.png',
+        price: 2
     },
     {
-        id:5,
-        name:'Electric',
-        image:'/car33.png',
-        price:2.1
+        id: 5,
+        name: 'Electric',
+        image: '/car33.png',
+        price: 2.1
     }
 ]
